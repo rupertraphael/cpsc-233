75ec016883a0c3abdf24a94c47993fc66fc5562b
Rupert Amodia <rupertraphael@live.com>
2020-03-04 16:05:04 -0700

    Do assignment 6.

---
+++ assignment6/src/application/ProjectCategory.java
@@ -0,0 +1,19 @@
+package application;
+
+/*****************************
+ * class ProjectCategory
+ * 
+ * This class represents the project grade of the course. 
+ * It contains the following instance variables:
+ * 
+ */
+
+
+public class ProjectCategory extends Category {
+	public ProjectCategory(double weight, double rawGrade)
+	{
+		super("Project", weight, new double[]{rawGrade});
+	}
+
+
+}
